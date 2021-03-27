e9cde4e6acfe745dfea47f41dc8b9bfe8e4d165a
BraxB <Braxb92@gmail.com>
2021-03-26 23:16:59 -0400

    began building search functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Paris&units=metric&appid=4354bae4bc4f80de34b0ce15453d2200"
+// var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Paris&units=metric&appid=4354bae4bc4f80de34b0ce15453d2200"
 
 function getApi(requestUrl) {
     fetch(requestUrl)
@@ -6,8 +6,15 @@ function getApi(requestUrl) {
         return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      return data;
     })
   }
-  
-  getApi(requestUrl);
+
+$("#searchBtn").on("click", function() {
+  var city = $("#searchInput").val();
+  var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=4354bae4bc4f80de34b0ce15453d2200"
+  var data = getApi(requestUrl);
+  $(".selectedCity").html(city);
+  debugger
+  $(".temp").html("Temperature: " + data.list[0].temp);
+})
